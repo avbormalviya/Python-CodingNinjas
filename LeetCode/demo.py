bdc2c7e0bdc3ead2c797dae7575e3982146c4fd2
Vimal <avbormalviya2006@gmail.com>
2025-03-10 21:33:23 +0530

    Files added to Repo

---
+++ LeetCode/demo.py
@@ -0,0 +1,26 @@
+
+# Problem Statement
+
+"""
+
+"""
+
+
+# Solution
+
+import sys
+
+
+class Solution:
+    def (self, ):
+
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+    arr = list(map(int, sys.stdin.readline().split()))
+
+    target = int(input())
+
+    sol = Solution()
+
+    ans = sol.()
